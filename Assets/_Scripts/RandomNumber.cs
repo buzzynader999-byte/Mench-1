7cc2fa647fb1e11b3c7c784782770f22ce7d6f48
nader <buzzynader999@gmail.com>
2025-08-12 09:38:47 +0330

    git init

---
+++ Assets/_Scripts/RandomNumber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+    public static class RandomNumber
+    {
+        public static int Pick()
+        {
+            return Random.Range(0, 7);
+        }
+    }
+}
